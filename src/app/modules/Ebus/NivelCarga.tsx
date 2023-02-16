7d15791551f21072667ae2a1ce0848cfc979884e
MarcialDiaz <marcial.diaz@syscaf.co>
2023-02-16 11:49:03 -0500

    Cambios

--- src/app/modules/Ebus/NivelCarga.tsx
+++ src/app/modules/Ebus/NivelCarga.tsx
@@ -1,29 +1,49 @@
 
 import moment from "moment";
+import { useState } from "react";
 import { TituloNivelCarga } from "../../../_start/helpers/Texts/textosPorDefecto";
 import { PageTitle } from "../../../_start/layout/core/PageData";
 import { Principal } from "./components/NivelCarga/principal";
 
-import { DataEventosTiempoClientes, NivelCargaProvider } from "./core/NivelCargaProvider";
+import { DataEventosTiempoClientes, Indicador, NivelCargaProvider } from "./core/NivelCargaProvider";
 
 
 type Props = {
 };
 
  const  NivelCarga: React.FC<Props> = () => {
+    const [tamaMapa, settamaMapa] = useState("50%");
+    const [tamaTabla, settamaTabla] = useState("50%");
+    const [Visible, setVisible]  = useState(true);
+     //Funcion  para la tabla 100% o 50%
+     const ExpandirContraerTabla = () =>{
+        //Tabla
+         if(tamaTabla == '50%')
+           settamaTabla("100%");
+         else
+           settamaTabla("50%");
+         //Mapa
+         if(tamaMapa == '50%')
+             settamaMapa('0%')
+         else
+             settamaMapa('50%')
+     }
     return(
             <NivelCargaProvider>
                 <PageTitle >{TituloNivelCarga}</PageTitle>
                 <div className="row">
                     <div className="col-sm-6 col-md-6 col-xs-6">
-                       
+                    <button className="btn btn-sm btn-primary" title="Expadir contraer tabla" onClick={ExpandirContraerTabla}><i className="bi-display"></i></button>
                     </div>
-                    <div className="col-sm-6 col-md-6 col-xs-6">
+                    <div className="col-sm-5 col-md-5 col-xs-5">
                         <DataEventosTiempoClientes>{moment().format("MYYYY").toString()}</DataEventosTiempoClientes>
+                    </div>
+                    <div  className="col-sm-1 col-md-1 col-xs-1">
+                         <Indicador Visible={Visible }></Indicador>
                     </div>
                 </div>
 
-                <Principal></Principal>
+                <Principal ExpandirContraerTabla={ExpandirContraerTabla} tamaTabla={tamaTabla} tamaMapa={tamaMapa} ></Principal>
             </NivelCargaProvider>
     )
 }
--- src/app/modules/Ebus/components/NivelCarga/principal.tsx
+++ src/app/modules/Ebus/components/NivelCarga/principal.tsx
@@ -15,47 +15,25 @@ import { Soc } from "./Soc";
 import { TablaNivelCarga } from "./TablaNivelCarga";
 import { Vehiculos } from "./Vehiculos";
 type Props = {
+    ExpandirContraerTabla: () =>void;
+    tamaTabla: string;
+    tamaMapa:string;
+
 };
-const  Principal: React.FC<Props> = () => {
+const  Principal: React.FC<Props> = ({ExpandirContraerTabla, tamaTabla, tamaMapa}) => {
    
     const {Visible, DatosMapa, dataTable,  ClienteSeleccionado, setdataTable, setDatosMapa} = useDataNivelCarga()
     const [show, setShow] = useState(false);
     const [showSoc, setShowSoc] = useState(false);
-    const [tamaMapa, settamaMapa] = useState("50%");
-    const [tamaTabla, settamaTabla] = useState("50%");
+
     //Para los clientes
     useEffect(() =>{
        setDatosMapa(DatosMapa);
     },[DatosMapa,dataTable])
  
-    //Funcion  para la tabla 100% o 50%
-    const ExpandirContraerTabla = () =>{
-       //Tabla
-        if(tamaTabla == '50%')
-          settamaTabla("100%");
-        else
-          settamaTabla("50%");
-        //Mapa
-        if(tamaMapa == '50%')
-            settamaMapa('0%')
-        else
-            settamaMapa('50%')
-    }
+   
     return(
             <NivelCargaProvider>
-                <div className="row">
-                    <div className="col-sm-6 col-md-6 col-xs-6">
-                        <button className="btn btn-sm btn-primary" title="Expadir contraer tabla" onClick={ExpandirContraerTabla}><i className="bi-display"></i></button>
-                    </div>
-                
-                    <div className="col-sm-5 col-md-5 col-xs-5">
-                        {/* <CargaListadoClientes/> */}
-                       {/* <DataEventosTiempoClientes>{moment().format("MYYYY").toString()}</DataEventosTiempoClientes> */}
-                    </div>
-                    <div className="col-sm-1 col-md-1 col-xs-1">
-                        <Indicador Visible={(Visible == undefined ? true:false)}></Indicador>
-                    </div>
-                </div>
                 <Soc show={showSoc} handleClose={function (): void {
                 setShowSoc(false);
                 } }/>
