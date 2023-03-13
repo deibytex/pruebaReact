8ace5bd1c1e9787686962b4ad069d2bb40d39788
Syscaf <66860235+Syscaf@users.noreply.github.com>
2023-03-13 08:55:52 -0500

    2023 03 13

--- src/app/modules/Ebus/components/NivelCarga/mapa.tsx
+++ src/app/modules/Ebus/components/NivelCarga/mapa.tsx
@@ -1,37 +1,54 @@
 import { Box, Typography } from "@mui/material";
-import MarkerClusterGroup from 'react-leaflet-cluster'
+
 import { Icon } from "leaflet";
 import { useEffect, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, Tooltip, useMap } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 import { useDataNivelCarga } from "../../core/NivelCargaProvider";
 import { MapaDTO } from "../../models/NivelcargaModels";
 
-const Mapa : React.FC =  () =>{
-   // datos del provider
-   const {DatosMapa, markerSeleccionado} = useDataNivelCarga()
-   const [map, setMap] = useState<MapaDTO[]>([]);
-   const [show, setshowp] = useState<boolean>(false);
-   const [activePark, setActivePark] = useState<MapaDTO>();
-    
-   // actiualiza la informacion de todos los vehiculos
+import L from "leaflet";
+import { toAbsoluteUrl } from "../../../../../_start/helpers";
+
+
+const Mapa: React.FC = () => {
+    // datos del provider
+    const { DatosMapa, markerSeleccionado } = useDataNivelCarga()
+    const [map, setMap] = useState<MapaDTO[]>([]);
+    const [show, setshowp] = useState<boolean>(false);
+    const [activePark, setActivePark] = useState<MapaDTO>();
+
+    const [centerLatitud, setcenterLatitud] = useState<number>(0);
+    const [centerLongitud, setcenterLongitud] = useState<number>(0);
+    const [zoom, setzoom] = useState<number>(13);
+    const [isClustering, setisClustering] = useState<boolean>(true);
+
+  
+    // actiualiza la informacion de todos los vehiculos
     useEffect(
         () => {
-            setMap((DatosMapa as MapaDTO[]));
+            let datosmapa = (DatosMapa as MapaDTO[])
+            setMap(datosmapa);
             setshowp(true)
+            setcenterLatitud(Number.parseFloat(datosmapa[0].latitud))
+            setcenterLongitud(Number.parseFloat(datosmapa[0].longitud))
         }
         , [DatosMapa]
-     );
+    );
 
-     // selecciona el marker
-     useEffect(
+    // selecciona el marker
+    useEffect(
         () => {
 
             setActivePark(markerSeleccionado);
-            console.log("markerSeleccionado", markerSeleccionado)
+   if((markerSeleccionado != undefined)) {
+                    setisClustering(false)
+                    setzoom(16)
+   }
         }
         , [markerSeleccionado]
-     );
-     //  whenCreated={setMap}
+    );
+    //  whenCreated={setMap}
     const here = {
         apiKey: 'h7cWVY3eEiZeilhreUhv07kKMJMizDl6elWoN7cb8wg'
     }
@@ -39,100 +56,156 @@ const Mapa : React.FC =  () =>{
     const style = 'reduced.night';
     const CapaBasicNight = `https://2.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/${style}/{z}/{x}/{y}/512/png8?apiKey=${here.apiKey}&ppi=320`;
     //const CapaHibrida = `https://2.traffic.maps.ls.hereapi.com/maptile/2.1/traffictile/newest/hybrid.traffic.day/{z}/{x}/{y}/512/png8?apiKey=${here.apiKey}&ppi=320`;
-   // const CapaTraficoDia = `https://2.traffic.maps.ls.hereapi.com/maptile/2.1/traffictile/newest/normal.traffic.day/{z}/{x}/{y}/512/png8?apiKey=${here.apiKey}&ppi=320`;
+    // const CapaTraficoDia = `https://2.traffic.maps.ls.hereapi.com/maptile/2.1/traffictile/newest/normal.traffic.day/{z}/{x}/{y}/512/png8?apiKey=${here.apiKey}&ppi=320`;
 
     const skater = new Icon({
         iconUrl: "/skateboarding.svg",
         iconSize: [25, 25]
     });
-   
-    
+
+
 
 
     //Funcion que ubica los iconos para el soc
-    const getIconSoc = (data:any) => {
+    const getIconSoc = (data: any) => {
         return (
-          (data <= 100 && data >= 80)? <><i className="bi-battery-full" style={{ color:'#00B050'}}></i><span style={{fontSize:"15px"}}> {data == null ? "" : data.toFixed(0)}%</span></>:
-           data <= 79 && data >= 60 ? <><i className="bi-battery-full rotate-45-verde" style={{color:'#92D050'}}></i><span style={{fontSize:"15px"}}>{data == null ? "" : data.toFixed(0)}%</span></>:
-          (data <= 59 && data >= 40 ? <><span><i className="bi-battery-half rotate-45-yellow" style={{fontSize:"15px", color:'#ff0'}}></i></span><span style={{fontSize:"15px"}}>{data == null ? "" : data.toFixed(0)}%</span></>:
-          (data <= 39 && data >= 20 ? <><span><i className="bi-battery-half rotate-45-naranja" style={{color:'#ffa500'}}></i></span><span style={{fontSize:"15px"}}>{data == null ? "" : data.toFixed(0)}%</span></>:
-          (data <= 19 && data >= 0 ? <><span><i className="bi-battery-half" ></i></span><span style={{fontSize:"15px"}}>{data == null ? "" : data.toFixed(0)}%</span></>:
-          <><span><i className="bi-battery-half"></i></span> <span style={{fontSize:"15px"}}>{data == null ? "" : data.toFixed(0)}%</span></>)))
+            (data <= 100 && data >= 80) ? <><i className="bi-battery-full" style={{ color: '#00B050' }}></i><span style={{ fontSize: "15px" }}> {data == null ? "" : data.toFixed(0)}%</span></> :
+                data <= 79 && data >= 60 ? <><i className="bi-battery-full rotate-45-verde" style={{ color: '#92D050' }}></i><span style={{ fontSize: "15px" }}>{data == null ? "" : data.toFixed(0)}%</span></> :
+                    (data <= 59 && data >= 40 ? <><span><i className="bi-battery-half rotate-45-yellow" style={{ fontSize: "15px", color: '#ff0' }}></i></span><span style={{ fontSize: "15px" }}>{data == null ? "" : data.toFixed(0)}%</span></> :
+                        (data <= 39 && data >= 20 ? <><span><i className="bi-battery-half rotate-45-naranja" style={{ color: '#ffa500' }}></i></span><span style={{ fontSize: "15px" }}>{data == null ? "" : data.toFixed(0)}%</span></> :
+                            (data <= 19 && data >= 0 ? <><span><i className="bi-battery-half" ></i></span><span style={{ fontSize: "15px" }}>{data == null ? "" : data.toFixed(0)}%</span></> :
+                                <><span><i className="bi-battery-half"></i></span> <span style={{ fontSize: "15px" }}>{data == null ? "" : data.toFixed(0)}%</span></>)))
         );
     }
 
 
-    return ( <> {(show ) && ( 
-    <MapContainer
-     id="mapcontainter" 
-     center={[Number.parseFloat(map[0].latitud), Number.parseFloat(map[0].longitud)]} zoom={12}
-  
-    className= ""
-><MarkerClusterGroup   chunkedLoading
-      >
-   
-    <TileLayer
-        url={CapaBasicNight}
-    />
-    {activePark && (
-        <Popup
-            position={[
-                Number.parseFloat(activePark.latitud),
-                Number.parseFloat(activePark.longitud)
-            ]}
-            onClose={() => {
-                let evento = {}
-               // setActivePark();
-            }}
-        >
-            <div className="card shadow-sm  border">
-                <div className="card-title fs-2 bg-secondary border "> <p className="text-center">{activePark.placa}</p></div>
-                <div className="card-body">
-                <Box
-                  sx={{
-                    display: 'grid',
-                    margin: 'auto',
-                    gridTemplateColumns: '1fr 1fr ',
-                    gridTemplateRows: '30px',
-                   
-                  }} 
-                >     
-                <Typography > Soc:</Typography>             
-                  <Typography >{getIconSoc(activePark.soc)}</Typography>       
-                  <Typography>Operador:</Typography>
-                  <Typography >{activePark.driver}</Typography>             
-                </Box>
-               
-                </div>
-                
-            </div>
-        </Popup>
-    )}
 
-
-     
-    { map.map(park => (
-        <Marker
-
-            key={park.assetId}
-            position={[
-                Number.parseFloat(park.latitud),
-                Number.parseFloat(park.longitud)
-            ]}
-            eventHandlers={{
-                click: (e: any) => {
-                    setActivePark(park);
-                },
-            }}
-
-
-
-        />
-    ))}
-      </MarkerClusterGroup>
-</MapContainer>)} </>
-       );    
+    function Puntos() {
+        const mapa = useMap();
       
+        return (<>
+            {map != undefined && map.length > 0 &&
+                map.map((park) => {
+                    return (
+                        <Marker
+                        title={park.placa}
+                           icon={ L.icon({
+                            iconUrl: toAbsoluteUrl('/media/syscaf/iconbus.png'),
+                            iconSize: [40, 40]
+                          
+                          }) }
+                            key={park.assetId}
+                            position={[
+                                Number.parseFloat(park.latitud),
+                                Number.parseFloat(park.longitud)
+                            ]}
+                            eventHandlers={{
+                                click: (e: any) => {                                   
+                                    setActivePark(park);
+                                },
+                            }}>
+
+<Tooltip className="bg-transparent border-0  text-white fs-8" direction="right" offset={[13, 0]} opacity={1} permanent>
+       {park.placa}
+      </Tooltip>
+
+                        </Marker>
+                    );
+                })}
+        </>
+
+        );
+    }
+
+    function RenderPopUp() {
+        const mapa = useMap();
+        return (<>
+            {activePark && (
+                <Popup
+                
+                    position={[
+                        Number.parseFloat(activePark.latitud),
+                        Number.parseFloat(activePark.longitud)
+                    ]}
+                    onClose={() => {
+                        setActivePark(undefined);
+                        mapa.setView(
+                            [
+                             centerLatitud,
+                                centerLongitud
+                            ],
+                            zoom
+                        );
+                       
+                        setisClustering(true)
+                    }}
+                    onOpen={() => {
+
+                        mapa.setView(
+                            [
+                                Number.parseFloat(activePark.latitud),
+                                Number.parseFloat(activePark.longitud)
+                            ],
+                            zoom
+                        );
+                    }}
+                >
+                    <div className="card shadow-sm  border">
+                        <div className="card-title fs-2 bg-secondary border "> <p className="text-center">{activePark.placa}</p></div>
+                        <div className="card-body">
+                            <Box
+                                sx={{
+                                    display: 'grid',
+                                    margin: 'auto',
+                                    gridTemplateColumns: '1fr 1fr ',
+                                    gridTemplateRows: '30px',
+
+                                }}
+                            >
+                                <Typography > Soc:</Typography>
+                                <Typography >{getIconSoc(activePark.soc)}</Typography>
+                                <Typography>Operador:</Typography>
+                                <Typography >{activePark.driver}</Typography>
+                            </Box>
+
+                        </div>
+
+                    </div>
+                </Popup>
+
+
+            )}
+        </>)
+    }
+
+
+
+
+    return (<> {(show) && (
+        <MapContainer
+            id="mapcontainter"
+            center={[centerLatitud, centerLongitud]} zoom={zoom}
+            className=" ml-4"
+        >
+            <TileLayer
+                url={CapaBasicNight}
+            />
+
+            <RenderPopUp />
+
+            {/**  si son todos aplicamos el clustering, si se filtra lo desagregamos*/}
+            {(isClustering) && (<MarkerClusterGroup>
+                <Puntos />
+            </MarkerClusterGroup>)}
+
+
+            {(!isClustering) && (
+                <Puntos />
+            )}
+
+        </MapContainer>)} </>
+    );
+
 }
 
-export {Mapa};
+export { Mapa };
